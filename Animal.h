b41c6ceba5263ee5827f347b472f85250b61def9
Abdullah Mahmoud Mohamed Emam <abdullah.emam@student.giu-uni.de>
2025-11-12 15:20:37 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,18 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#ifndef UNTITLED1_ANIMAL_H
+#define UNTITLED1_ANIMAL_H
 #include <string>
 using namespace std;
-
-//define animal class here 
-
-
-
-#endif
+class Animal
+{
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    public:
+    Animal();
+    Animal(int age, string name);
+    void display();
+    void feed();
+    ~Animal();
+};
+#endif //UNTITLED1_ANIMAL
