bf9760d6e6320d43d3798e7ec2a5c78fea23d0e2
Abdullah Mahmoud Mohamed Emam <abdullah.emam@student.giu-uni.de>
2025-11-12 15:24:35 +0200

    Rename header guard in Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,15 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#ifndef UNTITLED1_VISITOR_H
+#define UNTITLED1_VISITOR_H
 #include <string>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
-
+class Visitor
+{
+    private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor(string visitorName, int tick);
+    void displayInfo();
+};
 #endif
