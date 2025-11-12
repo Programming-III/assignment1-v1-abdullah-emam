3a569dd5bd9446fef7cb9eaa165cbce6cd9f3460
Abdullah Mahmoud Mohamed Emam <abdullah.emam@student.giu-uni.de>
2025-11-12 15:22:25 +0200

    Update Enclosure.h with class definition and guards
    Refactor header guard and define ENCLOSURE class.

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,17 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+#ifndef UNTITLED1_ENCLOSURE_H
+#define UNTITLED1_ENCLOSURE_H
 #include "Animal.h"
 
-//define enclosure class here 
-
-
-
-
-
-
-
-
+class ENCLOSURE
+{
+    private:
+    Animal *animals;
+    int capacity;
+    int currentCount;
+    public:
+    ENCLOSURE(int cap);
+    void addAnimal(Animal *animal);
+    void display();
+    ~ENCLOSURE();
+};
 #endif
