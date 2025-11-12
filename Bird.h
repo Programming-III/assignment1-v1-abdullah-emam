20b5b3a718446420149320ec362fbf75a5037e27
Abdullah Mahmoud Mohamed Emam <abdullah.emam@student.giu-uni.de>
2025-11-12 15:21:43 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,12 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+#ifndef UNTITLED1_BIRD_H
+#define UNTITLED1_BIRD_H
 #include "Animal.h"
 
-//define class bird here 
-
-
-
-
-
-
-
+class Bird : public Animal
+{
+    private:
+    float wingSpan;
+    public:
+    Bird(int age, string na,float ws);
+};
 #endif
