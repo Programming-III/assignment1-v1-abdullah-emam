e0892dff4f9588fe33b09ca8ae70a872306706b8
Abdullah Mahmoud Mohamed Emam <abdullah.emam@student.giu-uni.de>
2025-11-12 15:23:10 +0200

    Create Mammal class with constructor and attributes
    Defined the Mammal class with a constructor and furColor attribute.

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,14 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+#ifndef UNTITLED1_MAMMAL_H
+#define UNTITLED1_MAMMAL_H
+#include <string>
 
 #include "Animal.h"
 
-//define mammal class here
-
-
-
-
-
-
+class Mammal :public Animal
+{
+    private:
+    std::string furColor;
+    public:
+    Mammal(int age , string na,string fc);
+};
 #endif
